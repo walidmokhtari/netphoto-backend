25e582cd2eed906d4694c69246a89a912d9ade6e
Walid <walid.mokhtari@etudiant.univ-perp.fr>
2022-02-24 11:14:10 +0100

    UPDATE 24-02-2022 11:14

--- src/controllers/webhooks.controller.js
+++ src/controllers/webhooks.controller.js
@@ -1,10 +1,13 @@
-const stripe = require("stripe")(process.env.STRIPE_SK);
+const config = require("../configs");
+const stripeKey = config.stripe.key;
+const stripe = require("stripe")(stripeKey);
 const User = require('../models/user.model');
 
 exports.stripewebhook = (req, res) => {
 
   let data;
   let eventType;
+ console.log("stripe == ", stripeKey);
 
   const webhookSecret = process.env.WEBHOOKSECRET;
 
@@ -12,7 +15,7 @@ exports.stripewebhook = (req, res) => {
 
     let event;
     let signature = req.headers["stripe-signature"];
-
+    console.log("sign ==", signature);
     try {
       event = stripe.webhooks.constructEvent(
         req.body,
@@ -20,7 +23,7 @@ exports.stripewebhook = (req, res) => {
         webhookSecret
       );
     } catch (err) {
-      console.log(`⚠️  Webhook signature verification failed.`, err);
+      //console.log(`⚠️  Webhook signature verification failed.`, err);
       return res.sendStatus(400);
     }
     data = event.data;
